17055c4052da8cf7a3bf1f2609fa6b68c5d6c924
Martino Mensio <martino.mensio@open.ac.uk>
2019-11-12 12:47:21 +0000

    vader setup

---
+++ main.py
@@ -0,0 +1,15 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import re
+import string
+analyzer = SentimentIntensityAnalyzer()
+
+def sentiment_analyzer_scores(text):
+    score = analyzer.polarity_scores(text)
+    print(text)
+    print(score)
+
+
+if __name__ == "__main__":
+    sentiment_analyzer_scores('ok')
+    sentiment_analyzer_scores('good')
+    sentiment_analyzer_scores('excellent')
